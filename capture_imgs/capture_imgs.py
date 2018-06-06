ddd48e44136dc1c71f7ab5c1e8c1439499b7cb35
Austin Pursley <austin.t.pursley@gmail.com>
2018-06-06 09:34:06 -0500

    commenting scripts, narrowing user input

--- capture_imgs/capture_imgs.py
+++ capture_imgs/capture_imgs.py
@@ -1,3 +1,15 @@
+# capture_imgs.py
+# Austin Pursley
+# 6/6/2018
+# This script was used for imaging plants for a summer research project.
+
+# This script runs on a Raspberry Pi and is used to quickly capture images from
+# both a visible light (VL) camera and an infared (IR) camera. In the physical
+# setup, the cameras are right next to each other and I was taking pictures of
+# some plants, one by one, to gather data for a computer vision research
+# project. I using MobaXterm to establish an SSH into the Pi to run this program.
+# The thermal camera used is a FLIR Lepton, v.1.5, 50 degree, shutterless version
+
 import os
 import time
 import datetime
@@ -9,9 +21,8 @@ from subprocess import call
 import cv2 as cv
 import numpy as np
 
-# Verify the time is updated / correct
+#Verify the time is updated / correct
 print "Time is ", datetime.datetime.now()
-
 # Show both images
 camera = PiCamera()
 camera.resolution = (1024, 768)
@@ -22,14 +33,11 @@ camera.hflip = True
 camera.start_preview()
 # Camera warm-up time
 sleep(2)
-
-while(1):
-  print "taking images..."
+program_run = True
+while(program_run):
+  print "Capturing images..."
   # Capture the visible light image
-  ################################################################
   timestamp = datetime.datetime.now().strftime("%m%d%H%M%S")
-  #matches = [f for f in os.listdir('.') if f.endswith("%04d.pgm" % i)] 
-  
   i = -1
   for f in os.listdir('.'):
     if os.path.splitext(f)[1] == ".pgm":
@@ -45,35 +53,33 @@ while(1):
   new_ir_img_name  = timestamp + "_IR_IMG_%04d.pgm" % (i+1)
   os.rename(ir_img_name, new_ir_img_name)
   ir_img_name = new_ir_img_name
-  ##################################################################
   ir_img = cv.imread(ir_img_name, -1)
   vl_img = cv.imread(vl_img_name, -1)
-  
-  # For visualization, create normalized, resized thermal image
+  # For visualization, create normalized, larger thermal image
   norm_ir = np.zeros((60, 80))
   norm_ir = cv.normalize(ir_img, norm_ir, 255, 0, cv.NORM_MINMAX, dtype=cv.CV_8U)
   norm_ir_resize = cv.resize(norm_ir, (0,0), fx=10, fy=10, interpolation = cv.INTER_NEAREST)
   vis_ir_dir = "visualize_ir/" + os.path.splitext(ir_img_name)[0] + ".jpg"
   cv.imwrite(vis_ir_dir, norm_ir_resize)
-  
-  cv.namedWindow("GetFocus", cv.WINDOW_NORMAL);
+  # This window is called to make sure two images windows are activated.
+  cv.namedWindow("GetFocus", cv.WINDOW_NORMAL)
   img = np.zeros((60, 80))
-  cv.imshow("GetFocus", img);
-  cv.setWindowProperty("GetFocus", cv.WND_PROP_FULLSCREEN, 1);
-  cv.waitKey(1);
-  cv.setWindowProperty("GetFocus", cv.WND_PROP_FULLSCREEN, cv.WINDOW_NORMAL);
+  cv.imshow("GetFocus", img)
+  cv.setWindowProperty("GetFocus", cv.WND_PROP_FULLSCREEN, 1)
+  cv.waitKey(1)
+  cv.setWindowProperty("GetFocus", cv.WND_PROP_FULLSCREEN, cv.WINDOW_NORMAL)
   cv.destroyWindow("GetFocus")
-  
+  # Two windows for images
   cv.namedWindow('vl_win',cv.WINDOW_NORMAL)
   cv.resizeWindow('vl_win', 400, 400)
   cv.moveWindow('vl_win', 50, 100)
   cv.imshow('vl_win', vl_img)
-  
   cv.namedWindow('ir_win',cv.WINDOW_NORMAL)
   cv.resizeWindow('ir_win', 400, 400)
   cv.moveWindow('ir_win', 450, 100)
   cv.imshow('ir_win', norm_ir)
-  
+  # Removing windows. Calling waitKey in succession removes an issue where
+  # windows would not close
   cv.waitKey(0)
   cv.destroyAllWindows()
   cv.waitKey(1)
@@ -81,18 +87,24 @@ while(1):
   cv.waitKey(1)
   cv.waitKey(1)
   cv.waitKey(1)
-  
-  ##################################################################
-  print "Capture: y Discard: n Escape: q"
-  k = raw_input("Enter: ")
-  if (k == 'y'):
-    print "taking more images..."
-  elif(k == 'n'):
-      print "deleting images, taking another"
-      os.remove(vl_img_name)
-      os.remove(ir_img_name)
-      os.remove(vis_ir_dir)
-  else:
-    break
+  # Capture will take another set of images
+  # Discard will delete previous images and take another set
+  # Quit will exit the program
+  print "Capture: c Discard: d Quit: q"
+  print "ENTER: "
+  while True:
+      k = raw_input()
+      if (k == 'y') | (k == 'c'):
+        print "Capturing more images..."
+        break
+      elif((k == 'n') | (k == 'd')):
+          print "Deleting images, capturing more..."
+          os.remove(vl_img_name)
+          os.remove(ir_img_name)
+          os.remove(vis_ir_dir)
+          break
+      elif k == 'q':
+            program_run = False
+            break
 camera.close()
 print "ending program..."
