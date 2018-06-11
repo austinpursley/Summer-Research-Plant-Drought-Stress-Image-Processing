210f045c4fa12fe803089be6a71c6181a1b1cc0e
Austin Pursley <austin.t.pursley@gmail.com>
2018-06-11 15:58:45 -0500

    misc

---
+++ archive/fix_dates.py
@@ -0,0 +1,29 @@
+# This script was used to change the name of the pictures I had taken of the
+# the plants on the first few days.
+
+import os
+
+day = '0602'
+time = ['0800]', '1000', '1200', '1400', '1600']
+type = ['front', 'top']
+vi   = ['', 'visualize_ir']
+
+for t in time:
+    for y in type:
+        for v in vi:
+            if v == '':
+                dir = t + '/' + y + '/'
+            elif v == 'visualize_ir':
+                dir = t + '/' + y + '/' + v + '/'
+            for f in os.listdir(dir):
+                if (os.path.splitext(f)[1] == ".pgm") | (os.path.splitext(f)[1] == ".jpg"):
+                    old_name = dir + f
+                    new_name = dir + day + t + "00" + f[-16:]
+                    print old_name
+                    print new_name
+                    if os.path.isfile(new_name):
+                        continue
+                    else:
+                        print "old name: ", old_name
+						print "new name: ", new_name, '/n'
+						os.rename(old_name, new_name)
